c5ba3a226629d433c3a800d5cf8f6f9206cfc40f
vijayskk <vijayskk007@gmail.com>
2021-12-16 13:22:02 +0530

    dotenv added

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express = require('express');
 
+var dotenv = require('dotenv');
+
 var indexRouter = require('./routes/index');
 
 var app = express();
-
+dotenv.config();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use('/', indexRouter);
 
+const port = process.env.PORT ?  process.env.PORT : 3000
 
 app.listen(3000,()=>{
-  console.log("App started");
+  console.log("App started in 3000");
 })
 
